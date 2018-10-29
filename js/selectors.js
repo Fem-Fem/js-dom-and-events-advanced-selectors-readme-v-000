c263ce3efb1435b299ef93d62641a7763cdcde45
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:43:15 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,6 +10,4 @@
     Write a function divSelector that does not accept any parameters. 
     The function should use a class selector to return the two divs with the class pics.
     $('.pics');
-    Write a function firstListItem that does not accept any parameters.
-    The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
     $('#pic-list ul li:first-child');
