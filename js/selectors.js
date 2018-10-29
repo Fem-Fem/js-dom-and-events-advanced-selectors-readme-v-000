7c668a661e5350d2a09e2a37851b93e060e47039
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:44:44 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,7 +2,9 @@
 
 
     Write a function paragraphSelector that does not accept any parameters. The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
-
+    function paragraphSelector() {
+    $('p');
+  }
     Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
 
     Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
