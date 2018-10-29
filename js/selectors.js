46fae3a5b15dcbbef92ac16285fccfd94762e85e
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:45:09 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,17 @@
 // declare your functions here...
-
-
-    Write a function paragraphSelector that does not accept any parameters. The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
     function paragraphSelector() {
     $('p');
-  }
-    Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
-
-    Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
-
-    Write a function divSelector that does not accept any parameters.
-    The function should use a class selector to return the two divs with the class pics.
-    $('.pics');
-    function myFunction(p1, p2) {
-    return p1 * p2;              // The function returns the product of p1 and p2
-}
+//   }
+//     Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
+//
+//     Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
+//
+//     Write a function divSelector that does not accept any parameters.
+//     The function should use a class selector to return the two divs with the class pics.
+//     $('.pics');
+//     function myFunction(p1, p2) {
+//     return p1 * p2;              // The function returns the product of p1 and p2
+// }
     function firstListItem() {
     $('#pic-list ul li:first-child');
   }
