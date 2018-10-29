5a155f3ca2c69949fbdf4018eed27b1c6286dba7
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 23:08:51 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,17 +1,17 @@
 function paragraphSelector() {
-  $('p');
+  return $('p');
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
