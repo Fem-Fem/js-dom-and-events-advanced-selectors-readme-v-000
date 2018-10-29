86b5c6b513e67045636d4ddc3c978aa34fc75019
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:47:27 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,11 +7,14 @@
   function ninjaBabySelector() {
     $('#baby-ninja');
   }
-//     Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
 //
 //     Write a function divSelector that does not accept any parameters.
 //     The function should use a class selector to return the two divs with the class pics.
 //     $('.pics');
+
+function ninjaBabySelector() {
+  $('#baby-ninja');
+}
 //     function myFunction(p1, p2) {
 //     return p1 * p2;              // The function returns the product of p1 and p2
 // }
