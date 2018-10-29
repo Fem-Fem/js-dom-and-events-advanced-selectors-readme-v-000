959012089abbfdcd4e1a39b3cd3ffdc899368011
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:47:38 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -13,7 +13,7 @@
 //     $('.pics');
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  $('.pics');
 }
 //     function myFunction(p1, p2) {
 //     return p1 * p2;              // The function returns the product of p1 and p2
