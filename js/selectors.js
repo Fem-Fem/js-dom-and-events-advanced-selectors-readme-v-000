ca5866df27d9ca99d34cfec31f46aa3fe244903c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:47:43 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,9 +4,9 @@
   }
 //     Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
 //
-  function ninjaBabySelector() {
-    $('#baby-ninja');
-  }
+function ninjaBabySelector() {
+  $('#baby-ninja');
+}
 //
 //     Write a function divSelector that does not accept any parameters.
 //     The function should use a class selector to return the two divs with the class pics.
