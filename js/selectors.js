7877f6086328230bd1693bb945b09f6f0a96636e
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 22:49:23 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -9,10 +9,6 @@ function lastImageSelector() {
 function ninjaBabySelector() {
   $('#baby-ninja');
 }
-//
-//     Write a function divSelector that does not accept any parameters.
-//     The function should use a class selector to return the two divs with the class pics.
-//     $('.pics');
 
 function divSelector() {
   $('.pics');
